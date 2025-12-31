4ec4655adfa2d8cd7a2591b6cc0da72a35d01ee9
giorgichkonidze <chkonigiorgi23@gmail.com>
2025-12-31 15:17:24 +0400

    commit

---
+++ group 25/level 169/classwork/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="bg-white shadow-lg text-center">
+        <h1 className="font-bold text-gray-800">
+        </h1>
+        <p className="text-gray-600">
+        </p>
+      </div>
+    </div>
+  )
+}
+export default App
