88e6df11931b463ce89f43b48192b0081620b008
giorgichkonidze <chkonigiorgi23@gmail.com>
2026-01-14 16:43:36 +0400

    commit

---
+++ group 25/level 167/classwork/project/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div className="w-50 h-25 bg-red-500"></div>
+  )
+}
+export default App
