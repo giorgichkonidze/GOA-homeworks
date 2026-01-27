d682f7d96d71b3e1d228548c227bb9b40edb47c3
giorgichkonidze <chkonigiorgi23@gmail.com>
2026-01-27 13:50:56 +0400

    commit

---
+++ group 25/level 174/homework/src/App.jsx
@@ -0,0 +1,36 @@
+import Button from "./Button"
+
+function App() {
+  return (
+    <div className='div1'>
+      <div className='bg-grey-900'>
+
+      </div>
+      <div className='bg-grey-800 '>
+        <img src='../src/assets/avatar-jessica.jpeg'/>
+        <h1>Jessica Randal</h1>
+        <h3>London United Kingdom</h3>
+        <h4>"Front-end developer and avid reader."</h4>
+        <Button name="GitHub" />
+        <Button name="Fronted Mentor" />
+        <Button name="Linkedin" />
+        <Button name="Twitter" />
+        <Button name="Instagram" />
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+// - Green: hsl(75, 94%, 57%)
+
+// - White: hsl(0, 0%, 100%)
+
+// - Grey 700: hsl(0, 0%, 20%)
+// - Grey 800: hsl(0, 0%, 12%)
+// - Grey 900: hsl(0, 0%, 8%)
---
+++ group 25/level 174/homework/src/Button.jsx
@@ -0,0 +1,7 @@
+function Button({name}) {
+    return (
+        <button className="bg-grey-700 text-white font-bold ">{name}</button>
+    )
+}
+
+export default Button;
