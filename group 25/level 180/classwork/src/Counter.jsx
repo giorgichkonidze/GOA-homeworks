c0637cc9542b0bc97eb76178a7729facd8d12fd9
giorgichkonidze <chkonigiorgi23@gmail.com>
2026-02-09 23:10:39 +0400

    commit

---
+++ group 25/level 180/classwork/src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>counter</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count +1)}>up</button>
+      <button onClick={() => setCount(count -1)}>down</button>
+      <button onClick={() => setCount(0)}>zero</button>
+    </div>
+
+  )
+}
+export default Counter
